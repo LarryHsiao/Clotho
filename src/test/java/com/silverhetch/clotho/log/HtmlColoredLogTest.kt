ca1dd78f112fd69f6af0152178e49dfc8aa46887
Larry Hsiao <larryhsiao@silverhetch.com>
2018-09-04 18:10:02 +0800

    HtmlColoredLog

---
+++ src/test/java/com/silverhetch/clotho/log/HtmlColoredLogTest.kt
@@ -0,0 +1,31 @@
+package com.silverhetch.clotho.log
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class HtmlColoredLogTest {
+
+    @Test
+    fun simple() {
+        with(HtmlColoredLog(object : Log {
+            override fun warning(message: String?) {
+                assertNotEquals("ABC", message)
+            }
+
+            override fun info(message: String?) {
+                assertNotEquals("ABC", message)
+            }
+
+            override fun error(message: String?) {
+                assertNotEquals("ABC", message)
+            }
+
+            override fun debug(message: String?) {
+                assertNotEquals("ABC", message)
+            }
+        })) {
+            info("ABC")
+            assertTrue(true)
+        }
+    }
+}
