b6cdedec02478413f8d951c43387cf183478ba02
Larry Hsiao <larryhsiao@silverhetch.com>
2019-02-14 09:23:51 +0800

    todo for refacetor
    Signed-off-by: Larry Hsiao <larryhsiao@silverhetch.com>

--- src/main/java/com/silverhetch/clotho/log/BeautyLog.kt
+++ src/main/java/com/silverhetch/clotho/log/BeautyLog.kt
@@ -4,6 +4,7 @@ import com.silverhetch.clotho.Source
 
 /**
  * Source that build a [Log] object with tag and color.
+ * @todo #refactor-1 replaced with class impalement [Log]
  */
 class BeautyLog : Source<Log> {
     override fun fetch(): Log {
