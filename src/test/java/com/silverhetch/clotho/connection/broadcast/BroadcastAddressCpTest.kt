ddd2218f67ea59da66c080516ca5ff7ef0f3c1ea
Larry Hsiao <larryhsiao@silverhetch.com>
2019-03-17 17:55:04 +0800

    doc for tests
    Signed-off-by: Larry Hsiao <larryhsiao@silverhetch.com>

--- src/test/java/com/silverhetch/clotho/connection/broadcast/BroadcastAddressCpTest.kt
+++ src/test/java/com/silverhetch/clotho/connection/broadcast/BroadcastAddressCpTest.kt
@@ -3,6 +3,9 @@ package com.silverhetch.clotho.connection.broadcast
 import org.junit.Assert.*
 import org.junit.Test
 
+/**
+ * Test for [BroadcastAddressCp]
+ */
 class BroadcastAddressCpTest {
     @Test
     fun simple() {
--- src/test/java/com/silverhetch/clotho/connection/broadcast/BroadcastAddressFirstTest.kt
+++ src/test/java/com/silverhetch/clotho/connection/broadcast/BroadcastAddressFirstTest.kt
@@ -3,6 +3,9 @@ package com.silverhetch.clotho.connection.broadcast
 import org.junit.Assert.*
 import org.junit.Test
 
+/**
+ * Test for [BroadcastAddressFirst]
+ */
 class BroadcastAddressFirstTest {
     @Test
     fun simple() {
