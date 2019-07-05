fc23bc0565817c68d1b90e9f977944114ec5d66c
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-07-05 09:28:54 +0800

    reduce MD5 class`s complexity

--- src/main/java/com/silverhetch/clotho/encryption/MD5.kt
+++ src/main/java/com/silverhetch/clotho/encryption/MD5.kt
@@ -6,44 +6,28 @@ import java.io.IOException
 import java.io.InputStream
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
-import javax.xml.bind.DatatypeConverter
 
 /**
  * Calculate MD5 of [InputStream].
- *
- *  @todo #1 Simplify this class
  */
 class MD5(private val input: InputStream) : Source<String> {
     override fun value(): String {
         try {
             val md = MessageDigest.getInstance("MD5")
 
-            try {
-                val buffer = ByteArray(1024)
-                try {
-                    while (true) {
-                        val read = input.read(buffer)
-                        if (read > 0) {
-                            md.update(buffer, 0, read)
-                        } else {
-                            break
-                        }
-                    }
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-                try {
-                    input.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-                return HexString(md.digest()).value()
-            } catch (e: FileNotFoundException) {
-                e.printStackTrace()
+            val buffer = ByteArray(1024)
+            var read = input.read(buffer)
+            while (read > 0) {
+                md.update(buffer, 0, read)
+                read = input.read(buffer)
             }
+            input.close()
+            return HexString(md.digest()).value()
         } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
             e.printStackTrace()
         }
         return ""
