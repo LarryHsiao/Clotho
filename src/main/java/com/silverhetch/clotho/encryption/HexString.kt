24b0da9cade3b99dad6f0361ddf595f93c8a5e08
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-07-05 17:41:26 +0800

    v1.8.2
    - fix build failed

--- src/main/java/com/silverhetch/clotho/encryption/HexString.kt
+++ src/main/java/com/silverhetch/clotho/encryption/HexString.kt
@@ -6,10 +6,10 @@ import java.util.*
 /**
  * Convert to hex from given data.
  */
-class HexString(private val bytes: Source<ByteArray>) : Source<String> {
+class HexString(private val bytes: ByteArray) : Source<String> {
     override fun value(): String {
         with(Formatter()) {
-            for (byte in bytes.value()) {
+            for (byte in bytes) {
                 format("%02x", byte)
             }
 
