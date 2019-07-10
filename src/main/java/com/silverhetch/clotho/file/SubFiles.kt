2efa16cc66519b2df778ce7986fea96e88332b4a
LarryHsiao <larryhsiao@larryhsiao.com>
2019-07-10 13:45:28 +0800

    SubFiles

---
+++ src/main/java/com/silverhetch/clotho/file/SubFiles.kt
@@ -0,0 +1,18 @@
+package com.silverhetch.clotho.file
+
+import com.silverhetch.clotho.Source
+import java.io.File
+import java.lang.IllegalArgumentException
+
+/**
+ * Source to generate files of given root.
+ */
+class SubFiles(private val root:File) : Source<List<File>> {
+    override fun value(): List<File> {
+        return if (root.isDirectory){
+            root.listFiles()?.toList()?: listOf()
+        }else{
+            throw IllegalArgumentException("the given file is not a directory")
+        }
+    }
+}
---
+++ src/test/java/com/silverhetch/clotho/file/SubFilesTest.kt
@@ -0,0 +1,37 @@
+package com.silverhetch.clotho.file
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.lang.IllegalArgumentException
+import java.nio.file.Files
+
+/**
+ * Test for com.silverhetch.clotho.file.SubFiles
+ */
+class SubFilesTest{
+    /**
+     * Check the result
+     */
+    @Test
+    fun simple() {
+        val root = Files.createTempDirectory("abc")
+        val child = Files.createTempFile(root.toAbsolutePath(), "abc","abcd")
+        assertEquals(
+            child.toFile(),
+            SubFiles(root.toFile()).value()[0]
+        )
+    }
+
+    @Test
+    fun notDirException() {
+        try{
+            SubFiles(Files.createTempFile(
+                "1",
+                ""
+            ).toFile()).value()
+            fail()
+        }catch (e:IllegalArgumentException){
+            assertTrue(true)
+        }
+    }
+}
