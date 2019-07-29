c1733f837d24248a25f43b83b9d9580068fd134c
LarryHsiao <larryhsiao@larryhsiao.com>
2019-07-30 00:28:53 +0800

    remove duplicate string

--- src/test/java/com/silverhetch/clotho/file/FileDeleteTest.kt
+++ src/test/java/com/silverhetch/clotho/file/FileDeleteTest.kt
@@ -8,15 +8,18 @@ import java.nio.file.Files
  * Test for [com.silverhetch.clotho.file.FileDelete]
  */
 class FileDeleteTest {
+    companion object{
+        private const val PREFIX = "prefix"
+    }
     /**
      * Check directory, subdirectory and files is removed successfully.
      */
     @Test
     fun simple() {
-        val file = Files.createTempDirectory("prefix").also { root ->
-            Files.createTempFile(root, "prefix", "")
-            Files.createTempDirectory(root, "prefix").also { subdir ->
-                Files.createTempFile(subdir, "prefix", "")
+        val file = Files.createTempDirectory(PREFIX).also { root ->
+            Files.createTempFile(root, PREFIX, "")
+            Files.createTempDirectory(root, PREFIX).also { subdir ->
+                Files.createTempFile(subdir, PREFIX, "")
             }
         }.toFile()
         FileDelete(file).fire()
