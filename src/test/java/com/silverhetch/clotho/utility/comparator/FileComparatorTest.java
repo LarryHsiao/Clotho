d77ce596c134275959a00e75aa5e9b99e85adabe
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-12-06 17:18:21 +0800

    quality check
    - Coding style
    - Test coverage

--- src/test/java/com/silverhetch/clotho/utility/comparator/FileComparatorTest.java
+++ src/test/java/com/silverhetch/clotho/utility/comparator/FileComparatorTest.java
@@ -2,11 +2,11 @@ package com.silverhetch.clotho.utility.comparator;
 
 import static org.junit.Assert.*;
 
-import com.silverhetch.clotho.utility.comparator.FileComparator;
 import org.junit.Test;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Comparator;
 
 /**
  * Unit-test for the class {@link FileComparator}
@@ -26,5 +26,11 @@ public class FileComparatorTest {
         assertEquals(1, new FileComparator().compare(file, dir));
         assertEquals(0, new FileComparator().compare(file, file2));
         assertEquals(0, new FileComparator().compare(dir, dir2));
+        assertEquals(0, new FileComparator(new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                return 0;
+            }
+        }).compare(dir, dir2));
     }
 }
