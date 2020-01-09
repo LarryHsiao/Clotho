28f7a46e98aec6f98ec559734929a117eb7f7830
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-01-09 11:39:26 +0800

    remove HttpRequest related classes

--- src/main/java/com/silverhetch/clotho/log/BeautyLog.kt
+++ src/main/java/com/silverhetch/clotho/log/BeautyLog.kt
@@ -4,7 +4,6 @@ import com.silverhetch.clotho.Source
 
 /**
  * Source that build a [Log] object with tag and color.
- * @todo #refactor-1 replaced with class impalement [Log]
  */
 class BeautyLog : Source<Log> {
     override fun value(): Log {
