e37c258a9c8d3f58f33d04ba7031c14ce7dba31a
Larry Hsiao <larryhsiao@larryhsiao.com>
2021-07-14 19:30:12 +0800

    test for BroadcastConnTest.kt

--- src/test/java/com/larryhsiao/clotho/connection/broadcast/BroadcastConnTest.kt
+++ src/test/java/com/larryhsiao/clotho/connection/broadcast/BroadcastConnTest.kt
@@ -39,4 +39,36 @@ class BroadcastConnTest {
         conn.close()
         Assertions.assertTrue(String(packet.data, UTF_8).startsWith("ping"))
     }
+
+    /**
+     * Send ping with buffer too small.
+     * Just receive all the buffer can store.
+     */
+    @Test
+    fun tooSmallBuffer() {
+        val countDownLatch = CountDownLatch(1)
+        var packet = DatagramPacket(ByteArray(0), 0)
+        val conn = BroadcastConn(
+            DatagramSocket(8888, InetAddress.getByName("localhost")).apply {
+                broadcast = true
+                soTimeout = 1000
+            },
+            1,
+            ConstSource(InetAddress.getByName("localhost")),
+            8888
+        ) { incoming ->
+            packet = incoming
+            countDownLatch.countDown()
+        }.apply {
+            launch()
+            send("ping")
+        }
+        conn.launch()
+        countDownLatch.await(5, TimeUnit.SECONDS)
+        conn.close()
+        Assertions.assertEquals(
+            "p",
+            String(packet.data, UTF_8)
+        )
+    }
 }
